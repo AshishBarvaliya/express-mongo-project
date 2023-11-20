4326218037562e7f0654703d5fce07c42cae470c
Ashish Baravaliya <vier.institute35@gmail.com>
2023-11-20 10:36:37 -0500

    feat: endpoints added

---
+++ index.js
@@ -0,0 +1,64 @@
+var express = require("express");
+var mongoose = require("mongoose");
+let path = require("path");
+
+var app = express();
+app.use(express.json());
+
+var productSchema = mongoose.Schema({
+  id: Number,
+  name: String,
+  price: Number,
+  color: String,
+  manufacturer: String,
+  startingDateAvailable: String,
+  endingDateAvailable: String,
+  image: String,
+  description: String,
+});
+var Product = mongoose.model("Product", productSchema);
+
+mongoose
+  .connect("mongodb://localhost/Products")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+app.get("/", function (req, res) {
+  res.send("Hello world!");
+});
+
+app.get("/products/:id/:field", async (req, res) => {
+  try {
+    // products/identifiers
+    if (req.params.id === "identifiers") {
+      const products = await Product.find();
+      return res.send(products.map((e) => ({ id: e.id })));
+    }
+
+    // products/images/{filename}
+    if (req.params.id === "images" && req.params.field) {
+      return res.sendFile(`images/${req.params.field}`, { root: __dirname });
+    }
+
+    // products/:id
+    if (req.params.id) {
+      const product = await Product.findOne({ id: req.params.id }).exec();
+      if (!product) return res.status(404).send("Product not found");
+
+      // products/:id/field
+      if (req.params.field) {
+        return res.send({ [req.params.field]: product[req.params.field] });
+      } else {
+        return res.send(product);
+      }
+    }
+
+    // products
+    const products = await Product.find();
+    res.send(products);
+  } catch (err) {
+    res.status(500).send("Error fetching product");
+  }
+});
+
+app.listen(3000);
